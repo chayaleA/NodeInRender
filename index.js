6677dda7a3fe26ce5929eeaed830cc0e6cd9bd1c
chayaleA <chayale96@gmail.com>
2024-05-05 06:59:40 +0300

    commit2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express'  
+import express from 'express'
 import cors from "cors"
 import bodyParser from "body-parser";
 import fetch from 'node-fetch';
 
 const app = express()
 
-const RENDER_API_KEY = 'rnd_6P9iImcHMO8eeP0PCoLMhbU88bfj' 
+const RENDER_API_KEY = 'rnd_6P9iImcHMO8eeP0PCoLMhbU88bfj'
 
 app.use(cors());
 
@@ -14,26 +14,27 @@ app.use(bodyParser.json());
 const port = 3000
 
 app.get('/', async (req, res) => {
-  try {
-      const renderResponse = await fetch('https://api.render.com/v1/services?limit=20', {
-          headers: {
-              'Authorization': `Bearer ${RENDER_API_KEY}`
-          }
-      });
+    try {
+        const renderResponse = await fetch('https://api.render.com/v1/services?limit=20', {
+            headers: {
+                'header': 'accept: application/json',
+                'Authorization': `Bearer ${RENDER_API_KEY}`
+            }
+        });
 
-      if (!renderResponse.ok) {
-          throw new Error('Failed to fetch data from Render API');
-      }
-      const renderApps = await renderResponse.json();
-      res.json(renderApps);
-  } catch (error) {
-      console.error('Error fetching data:', error);
-      res.status(500).json({ error: 'Failed to fetch data' });
-  }
+        if (!renderResponse.ok) {
+            throw new Error('Failed to fetch data from Render API');
+        }
+        const renderApps = await renderResponse;
+        res(renderApps);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500)('error: Failed to fetch data');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
+    console.log(`Example app listening on http://localhost:${port}`)
 })
 
 
